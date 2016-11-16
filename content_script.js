a0a677d38a81a6b3c14a832ac8ffdedcd4440848
mortalcat <mortalcat@hotmail.com>
2016-11-16 18:27:44 +0800

    Modified 161116, 6.27PM, content_Script modifired for basic functions

--- content_script.js
+++ content_script.js
@@ -3,47 +3,88 @@
  * Created by Shaocong on 11/16/2016.
  */
 
-
+//TOdo: OPEN A TAB AT BACK
 $(function() {
     console.log("here goes the injected content script");
-   // $("body").css({"background-color":"red","color":"blue"});
 
-    ///setTimeout( function () {
-        //test if we can retrieve DOM ele
+    ////test
 console.log($("frame[name='bottomFrame']"));
 
+
+    var credentials = {
+      "FIN":"G1086979Q",
+        "NoOfPeople":"1",
+        "contact":"82118301"
+
+    };
+
     $("frame[name='bottomFrame']").on("load", function() {
+        var reloadCounter = 0;
         console.log("The outer frame iS READY::");
         $("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").on("load", function() {
 
-            console.log("Here comes the inner frame" ); // nothing at first
-            console.log($("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("form[name='calendarForm']")  ); // nothing at first
-            console.log($("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("select[name='apptDetails.apptType']")  ); // nothing at first
+            reloadCounter++;
+            console.log(reloadCounter+" th time reload");
+            //console.log("Here comes the inner frame" );
+           // console.log($(this).contents().find("select[name='apptDetails.apptType']")  );
 
-            if(            $("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("select[name='apptDetails.apptType']").val()!="PRAP") {
-                $("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("select[name='apptDetails.apptType']").val("PRAP");
-                $("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("form[name='calendarForm']").submit();
+            //choose for application TYpe if it is not already chosen
+            if(reloadCounter==1 &&  $(this).contents().find("select[name='apptDetails.apptType']").val()!="PRAP") {
+                console.log("Now choose app type");
+                $(this).contents().find("select[name='apptDetails.apptType']").val("PRAP");
+               $(this).contents().find("form[name='calendarForm']").submit();
             }
+
+            if(reloadCounter==2)
+            {
+                ///TODO: how do we know if the page is already refreshed?
+
+                console.log($(this).contents().find("input[name='apptDetails.identifier1']"));
+
+
+                        //action="/ibook/publicLogin.do"
+
+
+                        if ($(this).contents().find("input[name='apptDetails.identifier1']").val()!=credentials.FIN ) {
+
+                            $(this).contents().find("input[name='apptDetails.identifier1']").val(credentials.FIN);
+                        }
+                if ($(this).contents().find("input[name='apptDetails.identifier2']").val()!=credentials.NoOfPeople ) {
+
+                    $(this).contents().find("input[name='apptDetails.identifier2']").val(credentials.NoOfPeople);
+                }
+                if ($(this).contents().find("input[name='apptDetails.identifier3']").val() !=credentials.contact) {
+
+                    $(this).contents().find("input[name='apptDetails.identifier3']").val(credentials.contact);
+                }
+
+
+                $(this).contents().find("form[name='calendarForm'][action='/ibook/publicLogin.do']").submit();
+
+            } else if(reloadCounter==3){
+
+                //has avaialble date?
+                //TODO:automatically choose for you
+                if($(this).contents().find("td.cal_AS").length){
+                    console.log($(this).contents().find("td.cal_AS"));
+///TODO:this click does not work, find out why
+                    $(this).contents().find("td.cal_AS")[0].click();
+                }
+//TODO:4th page submit
+                //TODO: check if date is the exact next day, if so, do not book
+
+            }
+
+
+
+
+
+
+
         });
-//TODO: the website will reload, make sure not choose again afte reload
 
     });
 
-        //console.log($("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents() ); // nothing at first
-       // console.log($("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("body")  ); // nothing at first
-
-      // $("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("body").css({"background-color":"red","color":"blue"}); // nothing at first
-
-     // console.log($("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("form[name='calendarForm']")  ); // nothing at first
-      //  console.log($("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("select[name='apptDetails.apptType']")  ); // nothing at first
-
-       // $("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("select[name='apptDetails.apptType']").val("PRAP");
-      // $("frame[name='bottomFrame']").contents().find("frame[name='mainFrame']").contents().find("form[name='calendarForm']").submit();
-    ////}, 10000 );////wait for frame to load
-
-    // $("#calendarForm", $("#bottomFrame").contents()).css({"background-color":"red","color":"blue"}).val("PRAP");
-//jQuery("#calendarForm").css({"background-color":"red"});
-//jQuery("#gaia_loginform").submit();
-
+///todo:notification
 });
 
