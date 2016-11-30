2b39d613a7baa5a47375df1207f8535b796f7d40
mortalcat <mortalcat@hotmail.com>
2016-11-30 18:05:22 +0800

    Modified - adding Save receipt function. Not finished.

--- background.js
+++ background.js
@@ -7,12 +7,6 @@
 
 
 
-function showNotification(title, content) {
-
-    new Notification(title, {
-        body: content
-    });
-}
 
 chrome.browserAction.onClicked.addListener(function(atab) {
 //
@@ -27,6 +21,63 @@ chrome.browserAction.onClicked.addListener(function(atab) {
 
 
                           chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+
+                              function showNotification(title, content) {
+
+                                  new Notification(title, {
+                                      body: content
+                                  });
+                              }
+
+
+                              function getBlob(dataURL){
+                                  // convert base64 to raw binary data held in a string
+                                  // doesn't handle URLEncoded DataURIs
+                                  var byteString = atob(dataURI.split(',')[1]);
+
+                                  // separate out the mime component
+                                  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+                                  // write the bytes of the string to an ArrayBuffer
+                                  var ab = new ArrayBuffer(byteString.length);
+                                  var ia = new Uint8Array(ab);
+                                  for (var i = 0; i < byteString.length; i++) {
+                                      ia[i] = byteString.charCodeAt(i);
+                                  }
+
+                                  // create a blob for writing to a file
+                                  var blob = new Blob([ab], {type: mimeString});
+                                  return blob;
+                              }
+                              function saveBlob(blob, filename, callback, errback) {
+
+                                  function onwriteend() {
+                                      // open the file that now contains the blob - calling
+                                      // `openPage` again if we had to split up the image
+                                      //TODO: change
+                                      var urlName = ('filesystem:chrome-extension://' +
+                                      chrome.i18n.getMessage('@@extension_id') +
+                                      '/temporary/' + filename);
+
+                                      callback(urlName);
+                                  }
+
+                                  // come up with file-system size with a little buffer
+                                  //TODO: buffer required?
+                                  var size = blob.size + (1024 / 2);
+
+                                  // create a blob for writing to a file
+                                  var reqFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+                                  reqFileSystem(window.TEMPORARY, size, function(fs){
+                                      fs.root.getFile(filename, {create: true}, function(fileEntry) {
+                                          fileEntry.createWriter(function(fileWriter) {
+                                              fileWriter.onwriteend = onwriteend;
+                                              fileWriter.write(blob);
+                                          }, errback); // TODO - standardize error callbacks?
+                                      }, errback);
+                                  }, errback);
+                              };
 
                                   if(sender.tab.id!=tab.id) return;
                                         //console.log(request.type);
@@ -52,25 +103,43 @@ chrome.browserAction.onClicked.addListener(function(atab) {
                                       chrome.tabs.captureVisibleTab(
                                        tab.windowId, {format: 'png', quality: 100}, function(dataURI) {//getImage? Save it
                                        console.log("picture url: " +dataURI );
+                                              //TODO:put dataUrl into a file. save in local file system
+
+                                              var mBlob = getBlob(dataURI);
+                                              saveBlob(mBlob, "appointmentReceipt"+Date.now(), downloadPic, mErrback);
+                                              //TODO: download the url
+
+                                              function mErrBack() {
+
+                                              }
+                                              function downloadPic() {
+
+                                              }
+
+
 ///data URl format:
 ///data:image/png;base64,iVBORw0KGgoA
-                                      });
+                                                });
 
 
-       showNotification("A date for PR appointment has been booked for you",request.date);
+                                              showNotification("A date for PR appointment has been booked for you",request.date);
 
-        //todo:uncomment after testing
-        //******
-       //setTimeout( function () {
+                                                //todo:uncomment after testing
+                                                    //****** close the tab
+                                                     //setTimeout( function () {
 
-        //chrome.tabs.remove(tab.id,function () {
-          //  console.log("Now the tab is closed");
-        //});
-    //},5000);
+                                                        //chrome.tabs.remove(tab.id,function () {
+                                                      //  console.log("Now the tab is closed");
+                                                   //});
+                                                   //},5000);
 
-    }
+                                       }
 
-});
+
+
+
+
+                           });
 
 
                     });
